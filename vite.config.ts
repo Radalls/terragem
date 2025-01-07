aedfddf242eca2db1dea6ac62ef304a99c4556e4
Radalls <leosallard@gmail.com>
2025-01-07 19:23:12 +0100

    v0.5.1 (#10)

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,15 @@ export default defineConfig(({ mode }) => ({
             '@': path.resolve(__dirname, 'src'),
         },
     },
+    build: {
+        assetsInlineLimit: 0,
+        rollupOptions: {
+            output: {
+                manualChunks: undefined,
+                assetFileNames: 'assets/[name]-[hash][extname]'
+            }
+        }
+    },
     test: {
         globals: true,
         environment: 'jsdom',
